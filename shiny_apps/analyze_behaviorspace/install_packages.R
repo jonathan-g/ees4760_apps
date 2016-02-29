1385e669ac0218223b8725e5e8f7605813fa75f5
Jonathan Gilligan <jonathan.gilligan@vanderbilt.edu>
2016-02-29 12:55:20 -0600

    Update "install.packages.R"

--- shiny_apps/analyze_behaviorspace/install_packages.R
+++ shiny_apps/analyze_behaviorspace/install_packages.R
@@ -1,2 +1,4 @@
-install.packages(c('shiny','dplyr','stringr','tidyr','purrr','ggplot2'),
+install.packages(c('shiny','dplyr','stringr','tidyr','purrr','ggplot2',
+                   'readr', 'DT'),
+                 dependencies = TRUE,
                  repos = "https://cran.rstudio.com")
